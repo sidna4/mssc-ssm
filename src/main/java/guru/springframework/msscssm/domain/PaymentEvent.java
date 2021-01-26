803190a17fc70e566d12d526ed7cce022a787fc4
Sidney Antommarchi <sidna4@yahoo.com>
2021-01-26 17:54:48 -0500

    Lesson 203

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * User: sidna
+ * Date: 1/26/2021 5:52 PM
+ */
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINE, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentState.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * User: sidna
+ * Date: 1/26/2021 5:51 PM
+ */
+
+public enum PaymentState {
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
+}
