7aa6b7272ef7fa85c1540af549231749a5640198
Sidney Antommarchi <sidna4@yahoo.com>
2021-01-26 18:38:07 -0500

    Lesson 205

---
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.msscssm.config;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * User: sidna
+ * Date: 1/26/2021 6:25 PM
+ */
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+
+        sm.start();
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        System.out.println(sm.getState().toString());
+    }
+}
